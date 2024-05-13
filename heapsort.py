fd43d43767287315d3733a03ecbeb9564d607350
Raveniv <raveniv@ya.ru>
2024-05-13 22:37:37 +0500

    heapsort finished

--- heapsort.py
+++ heapsort.py
@@ -9,4 +9,15 @@ def heapify(arr, n, i):
         largest = r
     if largest != i:
         arr[i],arr[largest] = arr[largest],arr[i]
-    
+        heapify(arr, n, largest)
+        
+def sort(arr):
+    #print(arr) #check
+    n = len(arr)
+    for i in range(n, -1, -1):
+        heapify(arr, n, i)
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i] 
+        heapify(arr, i, 0)
+    #print(arr) #check
+    return arr
